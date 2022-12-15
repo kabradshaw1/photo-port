53966eebe1da312a06d3f46b0e8af0b54a84a9ae
kabradshaw1 <itllbok82@gmail.com>
2022-12-15 16:13:29 -0500

    finished updating nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { capitalizeFirstLetter } from '../../utils/helpers';
+
 
 function Nav(props) {
   const {
@@ -7,6 +8,10 @@ function Nav(props) {
     setCurrentCategory,
     currentCategory,
   } = props;
+  
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentCategory.name);
+  }, [currentCategory]);
 
   return (
     <header className="flex-row px-1">

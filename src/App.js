56c1f01b0821effbc7f86180ca21915c9b43c7f6
kabradshaw1 <itllbok82@gmail.com>
2022-12-15 10:56:28 -0500

    done with about and nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Nav from './components/Nav';
+import About from './components/About';
 
 function App() {
+
   return (
     <div>
+      <Nav />
       <main>
-        <About />
+          <About />        
       </main>
     </div>
   );
